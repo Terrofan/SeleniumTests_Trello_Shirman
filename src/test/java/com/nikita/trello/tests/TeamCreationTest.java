cb01539dd25800c93a23f4b9dc0ca4bc0c734b59
Nikita <Terrofan@users.noreply.github.com>
2020-01-19 21:13:53 +0200

    DataProvider

--- src/test/java/com/nikita/trello/tests/TeamCreationTest.java
+++ src/test/java/com/nikita/trello/tests/TeamCreationTest.java
@@ -1,11 +1,38 @@
 package com.nikita.trello.tests;
-
 import com.nikita.trello.models.TeamData;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class TeamCreationTest extends TestBase{
+    @DataProvider
+    public Iterator<Object[]> validTeams(){
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"name DP","description"});
+        list.add(new Object[]{"DPn",""});
+
+        return list.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validTeamsCSV() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/teamsPositiveCsv.csv")));
+        String line = reader.readLine();
+        while(line != null){
+            String[] split = line.split(",");
+            list.add(new Object[]{new TeamData().setTeamName(split[0]).setTeamDesc(split[1])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
     @BeforeMethod
     public void ensurePreconditions() throws InterruptedException {
         if (!appMan.getSessionHelper().isAvatarPresentOnHeader()) {
@@ -13,6 +40,35 @@ public class TeamCreationTest extends TestBase{
             appMan.getSessionHelper().fillLoginFormAtlassian("nikitashirman@gmail.com", "5457den5457");
             appMan.getHelperbase().pause(5000);
         }
+    }
+
+    @Test(dataProvider = "validTeams")
+    public void teamCreationTestFromHeaderWithDP(String teamName, String teamDesc) throws InterruptedException {
+        int teamCountBefore =  appMan.getTeamHelper().getTeamsCount();
+        appMan.getBoardHelper().clickOnPlusButton();
+        appMan.getTeamHelper().selectCreateTeamFromDropDown();
+        appMan.getTeamHelper().fillTeamCreationForm(new TeamData().setTeamName(teamName).setTeamDesc(teamDesc));
+        appMan.getTeamHelper().submitTeamCreation();
+        appMan.getTeamHelper().clickLaterButton();
+        appMan.getSessionHelper().returnToHomePage();
+        int teamCountAfter = appMan.getTeamHelper().getTeamsCount();
+        Assert.assertEquals(teamCountAfter, teamCountBefore + 1);
+        appMan.getSessionHelper().returnToHomePage();
+    }
+
+    @Test(dataProvider = "validTeamsCSV")
+    public void teamCreationTestFromHeaderWithDPCSV(TeamData team) throws InterruptedException {
+        int teamCountBefore =  appMan.getTeamHelper().getTeamsCount();
+        appMan.getBoardHelper().clickOnPlusButton();
+        appMan.getTeamHelper().selectCreateTeamFromDropDown();
+        appMan.getTeamHelper().fillTeamCreationForm(team);
+        appMan.getTeamHelper().submitTeamCreation();
+        appMan.getTeamHelper().clickLaterButton();
+        appMan.getSessionHelper().pause(3000);
+        appMan.getSessionHelper().returnToHomePage();
+        int teamCountAfter = appMan.getTeamHelper().getTeamsCount();
+        Assert.assertEquals(teamCountAfter, teamCountBefore + 1);
+        appMan.getSessionHelper().returnToHomePage();
     }
 
     @Test
